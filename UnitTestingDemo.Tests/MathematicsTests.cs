b1b3a5fe38124907ca1dc7fd0f931bee82691309
Antti Simonen <antti.simonen@gofore.com>
2017-06-26 15:25:06 +0300

    feat: added simple xunit c# demo

---
+++ UnitTestingDemo.Tests/MathematicsTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace UnitTestingDemo.Tests
+{
+    public class MathematicsTests
+    {
+        [Theory]
+        [InlineData(2, 5, 10)]
+        [InlineData(1, 5, 5)]
+        [InlineData(3, 2, 6)]
+        [InlineData(2, 0, 0)]
+        public void TestMultiplyReturnsCorrectValue(int x, int y, int expected)
+        {
+            var math = new Mathematics();
+            Assert.Equal(expected, math.Multiply(x, y));
+        }
+    }
+}
---
+++ UnitTestingDemo/Mathematics.cs
@@ -0,0 +1,15 @@
+﻿namespace UnitTestingDemo
+{
+    public class Mathematics
+    {
+        public int Multiply(int x, int y)
+        {
+            if (x == 0 || y == 0)
+            {
+                return 0;
+            }
+            
+            return x * y;
+        }
+    }
+}
